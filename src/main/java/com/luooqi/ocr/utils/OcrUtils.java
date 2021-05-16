0197228569fb5fe29122a39b77440790f23ebbea
wangkunyang <857996802@qq.com>
2021-05-16 10:34:34 +0800

    master分支的记录

--- src/main/java/com/luooqi/ocr/utils/OcrUtils.java
+++ src/main/java/com/luooqi/ocr/utils/OcrUtils.java
@@ -131,7 +131,7 @@ public class OcrUtils {
             return "";
         }
         System.out.println("===========");
-        System.out.println("===========这个是master分支的记录");
+        System.out.println("===========这个是master分支的记录1");
         JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("words_result");
         List<TextBlock> textBlocks = new ArrayList<>();
         boolean isEng = false;
